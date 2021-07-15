ce174c51dfe85149fb1572f008ab3571ca58c1d8
Brad Cornes <bradlc41@gmail.com>
2021-07-15 16:58:55 +0100

    update purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.vue"],
+  purge: [
+    "./components/**/*.{vue,js,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.js",
+    "./nuxt.config.ts"
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {}
